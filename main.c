cfb739aa76215d3e261ce4f2165cadc95354f2c9
AART1125 <AARToledo1125@gmail.com>
2024-11-24 22:40:01 +0800

    Fixed

--- main.c
+++ main.c
@@ -15,65 +15,54 @@ int main()
     float *finals = malloc(count * sizeof(float));
     float *times = malloc(count * sizeof(float));
     int *results = malloc(count * sizeof(float));
+    double timeExecuted = 0.0f;
     int access = 0;
 
+    srand(time(0));
     
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < 30; i++)
     {
-        float init = (rand() % (99 - 0 + 1)) + 0;
-        float final = (rand() % (200 - 50 + 1)) + 50;
-        float time = (rand() % 100) + 1;
+        for (int k = 0; k < count; k++)
+        {
+            inits[access] = (rand() % 101) + 0;
+            finals[access] = (rand() % 101) + 100;
+            times[access] = (rand() % 101) + 1;
 
-        printf("\n\nMatrix: %d\n\n", i+1);
-        printf("Initial Velocity (KM/H): %.2f\n", init);
-        printf("Final Velocity (KM/H): %.2f\n", final);
-        printf("Time (seconds): %.2f", time);
+            access++;
+        }
 
-        inits[access] = init;
-        finals[access] = final;
-        times[access] = time;
+        time_t start_time = time(NULL);
+        for (int j = 0; j < count; j++)
+        {
+            results[access - count + j] = asmaccelleration(inits[access - count + j], finals[access - count + j], times[access - count + j]);
+        }
+        time_t end_time = time(NULL);
+        timeExecuted += difftime(end_time, start_time);
+        if (i == 0)
+        {
+            float to_ms = 0.27777778f;
+            printf("\n\nOutput Correctness Check: \n");
+            for (int l = 0; l < access; l++)
+            {
+                float initVel = inits[l];
+                float finalVel = finals[l];
+                float timeTaken = times[l];
 
-        results[access] = asmaccelleration(init, final, time);
-        access++;
-    }
+                float expectedOutput = ((finalVel - initVel)*to_ms)/timeTaken;
 
-    printf("\n\nOutput Correctness Check: \n");
-    float to_ms = 0.27777778f;
-    for (int i = 0; i < access; i++)
-    {
-        float initVel = inits[i];
-        float finalVel = finals[i];
-        float timeTaken = times[i];
+                int actualOutput = results[l];
 
-        float expectedOutput = ((finalVel - initVel)*to_ms)/timeTaken;
-
-        int actualOutput = results[i];
-
-        printf("\nMatrix %d\n\n", i + 1);
-        printf("Input Initial Velocity: %.2f\n", initVel);
-        printf("Input Final Velocity: %.2f\n", finalVel);
-        printf("Input Time: %.2f\n", timeTaken);
-        printf("Expected Output: %d\n", (int)roundf(expectedOutput));
-        printf("Actual Output: %d\n", actualOutput);
+                printf("\nVector %d\n\n", l+1);
+                printf("Input Initial Velocity: %.2f\n", initVel);
+                printf("Input Final Velocity: %.2f\n", finalVel);
+                printf("Input Time: %.2f\n", timeTaken);
+                printf("Expected Output: %d\n", (int)roundf(expectedOutput));
+                printf("Actual Output: %d\n", actualOutput);
+            }
+        }
     }
     
-
-    // clock_t t;
-    // t = clock();
-    // for (int i = 0; i < 30; i++)
-    // {
-    //     asmaccelleration(10000.0, 99999.0, 10000.1);
-    // }
-    // t = clock() - t;
-    // printf("Result: %d", asmaccelleration(10000.0, 99999.0, 10000.1));
-    // double time = (double)t/CLOCKS_PER_SEC;
-    // printf("\nAverage time taken: %f", time);
-
-    // Average time taken after 30 runs = 0.0 seconds #asmaccelleration(0.0, 62.5, 10.1);
-    // Average time taken after 30 runs = 0.0 seconds #asmaccelleration(10.0, 99.0, 20.1);
-    // Average time taken after 30 runs = 0.0 seconds #asmaccelleration(100.0, 999.0, 20.1);
-    // Average time taken after 30 runs = 0.0 seconds #asmaccelleration(1000.0, 9999.0, 1000.1);
-    // Average time taken after 30 runs = 0.0 seconds #asmaccelleration(10000.0, 99999.0, 10000.1);
+    printf("\nExecution time: %f\n", timeExecuted);
 
     return 0;
 }
